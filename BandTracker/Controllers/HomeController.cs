bbcf2c70546e8d9a4097f3e9ef9954a659754467
Rakhee Gandhi <rpgandhi@gmail.com>
2017-11-08 11:29:41 -0800

    Start UI.

--- BandTracker/Controllers/HomeController.cs
+++ BandTracker/Controllers/HomeController.cs
@@ -12,5 +12,47 @@ namespace BandTracker.Controllers
       {
         return View();
       }
+
+      [HttpGet("/bands")]
+      public ActionResult Bands()
+      {
+        List<Band> allBands = Band.GetAll();
+        return view(allBands);
+      }
+
+      [HttpGet("/venues")]
+      public ActionResult Venues()
+      {
+        List<Venue> allVenues = Venue.GetAll();
+        return view(allVenues);
+      }
+
+      //NEW TASK
+        [HttpGet("/bands/new")]
+        public ActionResult BandForm()
+        {
+            return View();
+        }
+        [HttpPost("/bands/new")]
+        public ActionResult BandCreate()
+        {
+            Band newBand = new Band(Request.Form["band-name"]);
+            newBand.Save();
+            return View("Success");
+        }
+
+//NEW CATEGORY
+        [HttpGet("/venues/new")]
+        public ActionResult VenueForm()
+        {
+            return View();
+        }
+        [HttpPost("/venues/new")]
+        public ActionResult VenueCreate()
+        {
+            Venue newVenue = new Venue(Request.Form["venue-name"]);
+            newVenue.Save();
+            return View("Success");
+        }
     }
 }
